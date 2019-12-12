d6a16aa9f8e99f2b4459a8f2dd1994a8ca0b1813
Dialvive <diego.a.villalpando@ciencias.unam.mx>
2019-12-12 09:33:05 -0600

    ADD DDL Constraints

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -12,7 +12,9 @@ CREATE DATABASE Transportate;
 COMMENT ON DATABASE Transportate IS 
 'Base de datos sobre la empresa ficticia Transpórtate';
 
---ESTRUCURA DE TABLAS EN LA BASE DE DATOS
+------------------------------------------------------------
+--ESTRUCURA DE TABLAS EN LA BASE DE DATOS ------------------
+------------------------------------------------------------
 
 ----TABLAS CON LLAVES PRIMARIAS
 
@@ -68,8 +70,8 @@ COMMENT ON COLUMN chofer_fiscal.rfc IS
 COMMENT ON COLUMN chofer_fiscal.curp IS
 'Clave Única de Registro de Población. [Llave Secundaria]';
 COMMENT ON COLUMN chofer_fiscal.regimen_fiscal IS
-'Figura Fiscal: Persona Moral := '', Persona Física := '', o 
-Persona Física con Capacidad Empresarial :=''.';
+'Figura Fiscal: Persona Moral := M, Persona Física := F, o 
+Persona Física con Capacidad Empresarial := E.';
 
 --A1 o AUTOMÓVIL CUALIDAD
 CREATE TABLE automovil_cualidad (
@@ -164,10 +166,12 @@ COMMENT ON COLUMN servicio.puntos_generados IS
 'Cantidad de puntos abonados a la tarjeta del cliente en función
 de la cantidad.';
 
-----TABLAS CON LLAVES SECUNDARIAS SIN LLAVES PRIMARIAS
+------------------------------------------------------------
+----TABLAS CON LLAVES SECUNDARIAS SIN LLAVES PRIMARIAS -----
+------------------------------------------------------------
 
---TELEFONO
-CREATE TABLE telefono (
+--CLIENTE TELEFONO
+CREATE TABLE cliente_telefono (
     curp                CHAR(18)    NOT NULL,
     numero              varchar(13) NOT NULL
 );
@@ -180,19 +184,46 @@ COMMENT ON COLUMN telefono.curp IS
 COMMENT ON COLUMN telefono.numero IS
 'Número telefónico de clientes o choferes.';
 
---CORREO-E
-CREATE TABLE correo_e (
+--CLIENTE CORREO-E
+CREATE TABLE cliente_correo_e (
     curp                CHAR(18)    NOT NULL,
     direccion           TEXT        NOT NULL
 );
 COMMENT ON TABLE correo_e IS
 'Tabla que almacena las direcciones de correo electrónico de 
-clientes y choferes.';
+clientes.';
 COMMENT ON COLUMN correo_e.curp IS
-'Clave Única de Registro de Población de clientes o choferes.
+'Clave Única de Registro de Población de clientes.
 [Llave Secundaria]';
 COMMENT ON COLUMN correo_e.direccion IS
-'Dirección de correo electrónico de clientes o choferes.';
+'Dirección de correo electrónico de clientes.';
+
+--CHOFER TELEFONO
+CREATE TABLE chofer_telefono (
+    curp                CHAR(18)    NOT NULL,
+    numero              varchar(13) NOT NULL
+);
+COMMENT ON TABLE telefono IS
+'Tabla que almacena los números telefónicos de choferes.';
+COMMENT ON COLUMN telefono.curp IS
+'Clave Única de Registro de Población de choferes.
+[Llave Secundaria]';
+COMMENT ON COLUMN telefono.numero IS
+'Número telefónico de choferes.';
+
+--CHOFER CORREO-E
+CREATE TABLE chofer_correo_e (
+    curp                CHAR(18)    NOT NULL,
+    direccion           TEXT        NOT NULL
+);
+COMMENT ON TABLE correo_e IS
+'Tabla que almacena las direcciones de correo electrónico de 
+choferes.';
+COMMENT ON COLUMN correo_e.curp IS
+'Clave Única de Registro de Población de choferes.
+[Llave Secundaria]';
+COMMENT ON COLUMN correo_e.direccion IS
+'Dirección de correo electrónico de choferes.';
 
 --C3 o CHOFER DIRECCION
 CREATE TABLE chofer_direccion (
@@ -277,3 +308,327 @@ COMMENT ON COLUMN historial.curp IS
 COMMENT ON COLUMN historial.ids IS
 'Identificador serializable único de un servicio. 
 [Llave Secundaria]';
+
+------------------------------------------------------------
+--ASIGNACIÓN DE LLAVES SECUNDARIAS -------------------------
+------------------------------------------------------------
+
+----REFERENCIAS A CLIENTE
+ALTER TABLE cliente_correo_e 
+    ADD CONSTRAINT fk_cliente_correo_e_curp
+        FOREIGN KEY (curp)      REFERENCES cliente (curp);
+
+ALTER TABLE cliente_telefono
+    ADD CONSTRAINT fk_cliente_telefono_curp
+        FOREIGN KEY (curp)      REFERENCES cliente (curp);
+
+ALTER TABLE historial 
+    ADD CONSTRAINT fk_historial_curp
+        FOREIGN KEY (curp)      REFERENCES cliente (curp);
+
+ALTER TABLE tarjeta 
+    ADD CONSTRAINT fk_tarjeta_curp
+        FOREIGN KEY (curp)      REFERENCES cliente (curp);
+
+ALTER TABLE servicio 
+    ADD CONSTRAINT fk_servicio_curp
+        FOREIGN KEY (curp)      REFERENCES cliente (curp);
+
+----REFERENCIAS A CHOFER PERSONA
+ALTER TABLE chofer_correo_e 
+    ADD CONSTRAINT fk_chofer_correo_e_curp
+        FOREIGN KEY (curp)      REFERENCES chofer_persona (curp);
+
+ALTER TABLE chofer_telefono
+    ADD CONSTRAINT fk_chofer_telefono_curp
+        FOREIGN KEY (curp)      REFERENCES chofer_persona (curp);
+
+ALTER TABLE chofer_fiscal 
+    ADD CONSTRAINT fk_chofer_fiscal_curp
+        FOREIGN KEY (curp)      REFERENCES chofer_persona (curp);
+
+----REFERENCIAS A CHOFER FISCAL
+ALTER TABLE servicio
+    ADD CONSTRAINT fk_servicio_rfc
+        FOREIGN KEY (rfc)       REFERENCES chofer_fiscal (rfc);
+
+ALTER TABLE automovil_cualidad
+    ADD CONSTRAINT fk_automovil_cualidad_rfc
+        FOREIGN KEY (rfc)       REFERENCES chofer_fiscal (rfc);
+
+ALTER TABLE chofer_ref
+    ADD CONSTRAINT fk_chofer_ref_rfc
+        FOREIGN KEY (rfc)       REFERENCES chofer_fiscal (rfc);
+
+----REFERENCIAS A CHOFER REF
+ALTER TABLE chofer_direccion
+    ADD CONSTRAINT fk_chofer_direccion_calle
+        FOREIGN KEY (calle)     REFERENCES chofer_ref (calle);
+
+----REFERENCIAS A AUTOMÓVIL CUALIDAD
+ALTER TABLE servicio
+    ADD CONSTRAINT fk_servicio_placas
+        FOREIGN KEY (placas)    REFERENCES automovil_cualidad (placas);
+
+ALTER TABLE automovil_valor
+    ADD CONSTRAINT fk_automovil_valor_placas
+        FOREIGN KEY (placas)    REFERENCES chofer_fiscal;
+
+----REFERENCIAS A SERVICIO
+ALTER TABLE historial 
+    ADD CONSTRAINT fk_historial_ids
+        FOREIGN KEY (ids)       REFERENCES servicio (ids);
+
+------------------------------------------------------------
+--ASIGNACIÓN DE CHECKS--------------------------------------
+------------------------------------------------------------
+
+-----CLIENTE
+ALTER TABLE cliente
+    ADD CONSTRAINT ck_cliente_curp
+        CHECK (
+            curp ~ '^[A-Z]{1}[AEIOU]{1}[A-Z]{2}[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[HM]{1}(AS|BC|BS|CC|CS|CH|CL|CM|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)[B-DF-HJ-NP-TV-Z]{3}[0-9A-Z]{1}[0-9]{1}$'
+        );
+
+ALTER TABLE cliente
+    ADD CONSTRAINT ck_cliente_nombres
+        CHECK (
+            nombres ~ '\w' AND nombres !~ '\d'
+        );
+
+ALTER TABLE cliente
+    ADD CONSTRAINT ck_cliente_apellidos
+        CHECK (
+            apellidos ~ '\w' AND apellidos !~ '\d'
+        );
+ALTER TABLE cliente
+    ADD CONSTRAINT ck_cliente_fecha_nacimiento
+        CHECK (
+            fecha_nacimiento >'1900-01-01'
+        );
+
+-----CLIENTE CORREO E
+ALTER TABLE cliente_correo_e
+    ADD CONSTRAINT ck_cliente_correo_e_direccion
+        CHECK (
+            direccion ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'
+        );
+
+-----CLIENTE TELEFONO
+ALTER TABLE cliente_telefono
+    ADD CONSTRAINT ck_cliente_telefono_numero
+        CHECK (
+            numero ~ '\d' 
+            AND numero !~ '[[:alpha:]]' 
+            AND numero !~ '\s' 
+        );
+
+-----TARJETA
+ALTER TABLE tarjeta
+    ADD CONSTRAINT ck_tarjeta_distancia
+        CHECK (
+            distancia >= 0
+        );
+
+ALTER TABLE tarjeta
+    ADD CONSTRAINT ck_tarjeta_puntos
+        CHECK (
+            puntos >= 0
+        );
+
+ALTER TABLE tarjeta
+    ADD CONSTRAINT ck_tarjeta_num_viajes
+        CHECK (
+            num_viajes >= 0
+        );
+
+-----CHOFER PERSONA
+ALTER TABLE chofer_persona
+    ADD CONSTRAINT ck_cliente_curp
+        CHECK (
+            curp ~ '^[A-Z]{1}[AEIOU]{1}[A-Z]{2}[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[HM]{1}(AS|BC|BS|CC|CS|CH|CL|CM|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)[B-DF-HJ-NP-TV-Z]{3}[0-9A-Z]{1}[0-9]{1}$'
+        );
+
+ALTER TABLE chofer_persona
+    ADD CONSTRAINT ck_chofer_persona_nombres
+        CHECK (
+            nombres ~ '\w' AND nombres !~ '\d'
+        );
+
+ALTER TABLE chofer_persona
+    ADD CONSTRAINT ck_chofer_persona_apellidos
+        CHECK (
+            apellidos ~ '\w' AND apellidos !~ '\d'
+        );
+ALTER TABLE chofer_persona
+    ADD CONSTRAINT ck_chofer_persona_fecha_nacimiento
+        CHECK (
+            fecha_nacimiento >'1900-01-01'
+        );
+
+-----CHOFER CORREO E
+ALTER TABLE chofer_correo_e
+    ADD CONSTRAINT ck_chofer_correo_e_direccion
+        CHECK (
+            direccion ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'
+        );
+
+-----CHOFER TELEFONO
+ALTER TABLE chofer_telefono
+    ADD CONSTRAINT ck_chofer_telefono_numero
+        CHECK (
+            numero ~ '\d' 
+            AND numero !~ '[[:alpha:]]' 
+            AND numero !~ '\s' 
+        );
+
+-----CHOFER FISCAL
+ALTER TABLE chofer_fiscal
+    ADD CONSTRAINT ck_chofer_fiscal_rfc
+        CHECK (
+            rfc ~ '^([A-ZÑ\x26]{3,4}([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[A-Z|\d]{3})$'
+        );
+
+ ALTER TABLE chofer_fiscal
+    ADD CONSTRAINT ck_chofer_fiscal_regimen_fiscal
+        CHECK (
+            regimen_fiscal = 'M'
+            OR regimen_fiscal = 'F'
+            OR regimen_fiscal = 'E'
+        );
+       
+-----CHOFER REF
+ALTER TABLE chofer_ref
+    ADD CONSTRAINT ck_chofer_ref_calle
+        CHECK (
+            calle ~ '\w'
+        );
+
+-----CHOFER DIRECCION
+ALTER TABLE chofer_direccion
+    ADD CONSTRAINT ck_chofer_direccion_delegacion
+        CHECK (
+            delegacion ~ '\w'
+            AND delegacion !~ 'd'
+        );
+
+ALTER TABLE chofer_direccion
+    ADD CONSTRAINT ck_chofer_direccion_estado
+        CHECK (
+            estado ~ '\w'
+            AND estado !~ 'd'
+        );
+
+-----AUTOMOVIL CUALIDAD
+ALTER TABLE automovil_cualidad
+    ADD CONSTRAINT ck_automovil_placas
+        CHECK (
+            placas ~ '\w'
+            AND placas !~ '\d'
+        );
+
+ALTER TABLE automovil_cualidad
+    ADD CONSTRAINT ck_automovil_marca
+        CHECK (
+            marca ~ '\w'
+            AND marca !~ '\d'
+        );
+
+ALTER TABLE automovil_cualidad
+    ADD CONSTRAINT ck_automovil_modelo
+        CHECK (
+            modelo ~ '\w'
+        );
+
+ALTER TABLE automovil_cualidad
+    ADD CONSTRAINT ck_automovil_anio
+        CHECK (
+            anio !~ '\d'
+        );
+
+ALTER TABLE automovil_cualidad
+    ADD CONSTRAINT ck_automovil_color
+        CHECK (
+            color ~ '\w'
+            AND color !~ '\d'
+        );
+
+-----AUTOMOVIL VALOR
+ALTER TABLE
+    ADD CONSTRAINT ck_
+        CHECK (
+
+        );
+
+-----SERVICIO
+ALTER TABLE
+    ADD CONSTRAINT ck_
+        CHECK (
+
+        );
+
+ALTER TABLE
+    ADD CONSTRAINT ck_
+        CHECK (
+
+        );
+
+ALTER TABLE
+    ADD CONSTRAINT ck_
+        CHECK (
+
+        );
+
+ALTER TABLE
+    ADD CONSTRAINT ck_
+        CHECK (
+
+        );
+
+ALTER TABLE
+    ADD CONSTRAINT ck_
+        CHECK (
+
+        );
+
+ALTER TABLE
+    ADD CONSTRAINT ck_
+        CHECK (
+
+        );
+
+ALTER TABLE
+    ADD CONSTRAINT ck_
+        CHECK (
+
+        );
+
+ALTER TABLE
+    ADD CONSTRAINT ck_
+        CHECK (
+
+        );
+
+ALTER TABLE
+    ADD CONSTRAINT ck_
+        CHECK (
+
+        );
+
+ALTER TABLE
+    ADD CONSTRAINT ck_
+        CHECK (
+
+        );
+
+ALTER TABLE
+    ADD CONSTRAINT ck_
+        CHECK (
+
+        );
+
+ALTER TABLE
+    ADD CONSTRAINT ck_
+        CHECK (
+
+        );
